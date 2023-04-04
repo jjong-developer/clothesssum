3a4f67cfd5a023d4fb3c4f130e5fe0dc928e5aff
jongwook2 <72546383+jongwook2@users.noreply.github.com>
2023-04-04 20:49:15 +0900

    [작업 내용] - 회원가입 진행

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -14,13 +14,11 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import firebaseConfig from '../../firebase.config'
+import firebaseConfig from '../../firebase.config';
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-export { auth, db, storage };
+export const app = initializeApp(firebaseConfig);
+export const dbAuth = getAuth();
+export const dbService = getFirestore(app);
+export const dbStorage = getStorage(app);
 
 console.log("firebase db server load");
