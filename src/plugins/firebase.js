1533373a6aebd8adc783c030cbbc95e2ac048279
jongwook2 <72546383+jongwook2@users.noreply.github.com>
2023-04-26 04:03:21 +0900

    [작업 내용] - 회원가입, 로그인, 로그아웃 기능

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -10,8 +10,10 @@
 //
 // Vue.prototype.$firebase = firebase // 전역으로 사용하기 위함
 
+console.log("firebase db server load");
+
 import {initializeApp} from "firebase/app";
-import {getAuth} from "firebase/auth";
+import {getAuth, signInWithEmailAndPassword, onAuthStateChanged, sendPasswordResetEmail, GoogleAuthProvider, FacebookAuthProvider} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 import {getStorage} from "firebase/storage";
 import firebaseConfig from "../../firebase.config";
@@ -20,6 +22,5 @@ export const app = initializeApp(firebaseConfig);
 export const dbAuth = getAuth();
 export const dbService = getFirestore(app);
 export const dbStorage = getStorage(app);
-
-console.log("firebase db server load");
-
+export const googleProvider = new GoogleAuthProvider(); // 구글 간편 로그인 (https://console.cloud.google.com/ 개발자 사이트 등록)
+export const facebookProvider = new FacebookAuthProvider(); // 페이스북 간편 로그인 (https://developers.facebook.com/ 개발자 사이트 등록)
---
+++ src/views/Member/PasswordReset.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <Header></Header>
+
+        <div class="right-wrap">
+            <div class="sign-content-wrap">
+                <div class="sign-content">
+                    <h2>비밀번호 재설정</h2>
+                    <div>
+                        <label>이메일</label>
+                        <input id="userEmail" type="email" name="userEmail" value="" autocomplete="off" placeholder="" />
+                    </div>
+                    <div>
+                        <button class="wd-100 defalut-btn" type="button" @click="passwordResetSubmit();">이메일 보내기</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <Footer></Footer>
+    </div>
+</template>
+
+<script>
+import Header from "@/components/Common/Header";
+import Footer from "@/components/Common/Footer";
+import {emailCheck} from "@/assets/js/common.js";
+import {sendPasswordResetEmail} from "firebase/auth";
+import {dbAuth} from "@/plugins/firebase.js";
+
+export default {
+    name: "PasswordReset",
+
+    components: {
+        Header,
+        Footer,
+    },
+
+    data() {
+        return {
+
+        }
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+        passwordResetSubmit() {
+            let userEmail = document.querySelector('#userEmail');
+
+            if (!userEmail.value) {
+                alert('이메일을(를) 입력해주세요.');
+                userEmail.focus();
+                return;
+            } else if (!emailCheck(userEmail.value)) {
+                alert('이메일 형식이 올바르지 않습니다.');
+                userEmail.focus();
+                return;
+            } else {
+                sendPasswordResetEmail(dbAuth, userEmail.value).then(() => { // 비밀번호 재설정
+                    alert('해당 이메일로 링크를 전송하였습니다\n메일함을 확인해주세요.');
+                }).catch(error => {
+                    alert('잠시 후 다시 시도해주세요.');
+                });
+            }
+        }
+    },
+
+    watch: {
+
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
