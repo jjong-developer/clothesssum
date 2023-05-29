df9005e98bfa153c500b9a2f37151686d9ae2dac
jongwook2 <72546383+jongwook2@users.noreply.github.com>
2023-05-29 21:30:54 +0900

    [작업 내용] - 공지사항 게시글 수정 기능 완료

--- src/views/Board/Notice/WriteUpdate.vue
+++ src/views/Board/Notice/WriteUpdate.vue
@@ -9,17 +9,17 @@
             <div class="board-wrap">
                 <div class="board-write-wrap">
                     <label>작성자</label>
-                    <input id="author" type="text" name="author" :value="this.userName" placeholder="작성자를 입력해주세요." />
+                    <input id="author" type="text" name="author" :value="this.$route.query.author" placeholder="작성자를 입력해주세요." />
                     <label>제목</label>
-                    <input id="title" type="text" name="title" value="" placeholder="제목을 입력해주세요." />
+                    <input id="title" type="text" name="title" :value="decodeURI(this.$route.query.title)" placeholder="제목을 입력해주세요." />
                     <div class="write-tool">
                         <label>내용</label>
                         <button type="button">
-                            <img :src="require('@/assets/img/common/picture.png')" title="이미지 첨부" alt="이미지 첨부" />
+                            <img :src="require('@/assets/img/common/picture.png')" alt="이미지 첨부" />
                         </button>
                         <span>(이미지도 첨부하실 수 있습니다.)</span>
                     </div>
-                    <textarea id="content" value="" placeholder="내용을 입력해주세요."></textarea>
+                    <textarea id="content" :value="decodeURI(this.$route.query.contents)" placeholder="내용을 입력해주세요."></textarea>
                     <label>첨부파일</label>
                     <input class="file-find" type="file" />
                     <div class="board-btn-wrap">
@@ -37,8 +37,8 @@
 <script>
 import Header from "@/components/Common/Header";
 import Footer from "@/components/Common/Footer";
-import {goBack, siteReload, fileChange} from "@/assets/js/common.js";
-import {dbAddDoc, dbCollection, dbGetDocs, dbService} from "@/plugins/firebase.js";
+import {goBack, siteReload} from "@/assets/js/common.js";
+import {dbUpdateDoc, dbService, DOC} from "@/plugins/firebase.js";
 import {isUser} from "@/main";
 
 export default {
@@ -51,17 +51,6 @@ export default {
 
     data() {
         return {
-            isUser,
-            docID: '',
-            docData: '',
-            userName: '',
-            userEmail: '',
-            userPhoneNumber: '',
-            userPostCode: '',
-            userRoadAddress: '',
-            userJibunAddress: '',
-            userDetailAddress: '',
-            userUid: '',
             author: '', // 작성자
             title: '', // 제목
             contents: '', // 내용
@@ -78,64 +67,33 @@ export default {
     },
 
     mounted() {
-        this.getUsers();
-        // fileChange(); // 임시
+
     },
 
     methods: {
-        /**
-         * 회원 정보를 불러오기
-         */
-        async getUsers() {
-            this.usersQuery = await dbGetDocs(dbCollection(dbService, 'users'));
-
-            this.usersQuery.forEach((docs) => {
-                // console.log(doc.id);
-                console.log(docs.data());
-
-                this.docID = docs.id
-                this.docData = docs.data()
-                this.userUid = this.docData.uid;
-
-                if (isUser.uid === this.userUid) {
-                    this.userName = this.docData.name;
-                    this.userEmail = this.docData.email;
-                    this.userPhoneNumber = this.docData.phoneNumber;
-                    this.userPostCode = this.docData.postCode;
-                    this.userRoadAddress = this.docData.roadAddress;
-                    this.userJibunAddress = this.docData.jibunAddress;
-                    this.userDetailAddress = this.docData.detailAddress;
-                }
-            });
+        listUpdate() {
+            this.listUpdating();
         },
 
         /**
          * 게시글 수정
          */
-        listUpdate() {
+        async listUpdating() {
             this.author = document.querySelector('input[name=author]').value;
             this.title = document.querySelector('input[name=title]').value;
             this.contents = document.querySelector('#content').value;
 
-            // 첨부파일
-            // this.fileFind = document.querySelector('.file-find').files[0]; // 단일
-            // this.fileFind = [...document.querySelector('.file-find').files]; // 다중
-            // this.setSaveFile = ref(dbStorage, 'notice/file/' + this.fileFind.name);
-            // // this.setSaveFile = this.$firebase.storage().ref().child('notice/file/' + this.fileFind.name);
-            // this.getSaveFile = this.setSaveFile.put(this.fileFind);
-            // fileChange();
-
-            dbAddDoc(dbCollection(dbService, 'notice'), {
+            await dbUpdateDoc(DOC(dbService, 'notice', this.$route.query.docUID), {
                 'author': this.author, // 작성자
                 'title': this.title, // 제목
                 'contents': this.contents, // 내용
                 'date': new Intl.DateTimeFormat('ko', this.dateOptions).format(new Date()), // 등록일
             }).then(() => {
-                alert('게시글이 등록되었습니다.');
+                alert('게시글이 수정되었습니다.');
                 siteReload('/Board/Notice/List');
             }).catch((error) => {
                 console.log(error.message);
-                alert('게시글 등록중에 실패하였습니다.\n잠시 후 다시 시도해주세요.');
+                alert('게시글 수정중에 실패하였습니다.\n잠시 후 다시 시도해주세요.');
             });
         },
 
